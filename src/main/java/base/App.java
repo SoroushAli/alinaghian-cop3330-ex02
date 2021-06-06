ce1f44bd05965b6302295d040f440dc348ab3343
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-05 21:07:49 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,21 @@
+package base;
+/*
+Example Output
+
+What is the input string? Homer
+Homer has 5 characters.
+
+Constraints
+
+Be sure the output contains the original string.
+Use a single output statement to construct the output.
+Use a built-in function of the programming language to determine the length of the string.
+
+Challenges
+
+If the user enters nothing, state that the user must enter something into the program.
+Implement this program using a graphical user interface and update the character counter every time a key is pressed. If your language doesn’t have a particularly friendly GUI library, try doing this exercise with HTML and JavaScript instead.
+ */
+public class App {
+
+}
